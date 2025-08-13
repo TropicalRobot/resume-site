932d6ac33b55f0c45abccec35d37a294875df0a6
TropicalRobot <ryandeangriffiths@gmail.com>
2025-08-13 21:20:16 +0700

    Tools updated

--- src/components/SkillLogo.tsx
+++ src/components/SkillLogo.tsx
@@ -6,7 +6,7 @@ type SkillLogoProps = {
 }
 const SkillLogo: React.FC<SkillLogoProps> = ({ image, heading }) => {
     return (
-        <div className='col-span-1 flex flex-col items-center font-bold gap-4 justify-between'>
+        <div className='col-span-1 flex flex-col items-center font-bold gap-4 justify-between h-full'>
             <div className='flex flex-col justify-center h-full max-h-[64px]'>
                 <Image className='h-full' src={image} alt='Arrow' width={64} height={64} />
             </div>
